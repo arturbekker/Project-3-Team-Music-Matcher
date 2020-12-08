55ec1348903cc70d5381008dc52a1b4543d3bfe2
cflechilla <44211358+cflechilla@users.noreply.github.com>
2020-12-07 23:10:37 -0500

    Update MusicMatcher.cpp

--- MusicMatcher.cpp
+++ MusicMatcher.cpp
@@ -2,4 +2,16 @@ using namespace std;
 
 class MusicMatcher
 {
+  map<string, string> adjList;
+	ifstream fin("data.csv");
+	string artist;
+	string song;
+	while (fin.good())
+	{
+		getline(fin, artist, ']');
+		getline(fin, song, '\n');
+		adjList[song] = artist;
+		cout << artist << endl;
+	}
+	fin.close();
 }
